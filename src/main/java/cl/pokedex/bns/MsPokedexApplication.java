adc2c20714e65ba77c63cee93a6354321831deb6
unknown <oswaldo.jimenezhidalgo@gmail.com>
2021-03-07 11:54:51 -0300

    Initial commit

---
+++ src/main/java/cl/pokedex/bns/MsPokedexApplication.java
@@ -0,0 +1,13 @@
+package cl.pokedex.bns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPokedexApplication.class, args);
+	}
+
+}
